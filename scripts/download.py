36c6691688e8f0a33c60f8c9ebb73486380c806a
Allan Niles <allancoding.dev@gmail.com>
2024-05-20 19:18:29 -0600

    touch up

--- scripts/download.py
+++ scripts/download.py
@@ -8,13 +8,13 @@ def write_file(file_path, content):
     create_directory(os.path.dirname(file_path))
     with open(file_path, "w") as f:
         f.write(json.dumps(content, indent=4))
-        
-def download(urlpath, todl):
-    if os.path.exists("download/" + todl + ".json"):
+
+def complete(urlpath, todl):
+    if os.path.exists("complete/" + todl + ".json"):
         return
     solditems = requests.get(urlpath)
     data = solditems.json()
-    write_file("download/" + todl + ".json", data)
+    write_file("complete/" + todl + ".json", data)
 def read_json(file_path):
     with open(file_path, 'r') as f:
         data = json.load(f)
@@ -22,14 +22,14 @@ def read_json(file_path):
 
 urlpath = "https://openscriptureapi.org/api/scriptures/v1/lds/en/volume"
 
-download(urlpath + "s", "volumes")
+complete(urlpath + "s", "volumes")
 
-volumes = read_json("download/volumes.json")
+volumes = read_json("complete/volumes.json")
 for volume in volumes.get('volumes', []):
-    download(urlpath + "/" + volume["_id"], volume["_id"])
-    books = read_json("download/" + volume["_id"] + ".json")
+    complete(urlpath + "/" + volume["_id"], volume["_id"])
+    books = read_json("complete/" + volume["_id"] + ".json")
     for book in books.get('books', []):
-        download(urlpath + "/" + volume["_id"] + "/" + book["_id"], volume["_id"] + "/" + book["_id"])
-        chapters = read_json("download/" + volume["_id"] + "/" + book["_id"] + ".json")
+        complete(urlpath + "/" + volume["_id"] + "/" + book["_id"], volume["_id"] + "/" + book["_id"])
+        chapters = read_json("complete/" + volume["_id"] + "/" + book["_id"] + ".json")
         for chapter in chapters.get('chapters', []):
-            download(urlpath + "/" + volume["_id"] + "/" + book["_id"] + "/" + chapter["_id"].removeprefix(book["_id"]), volume["_id"] + "/" + book["_id"] + "/" + chapter["_id"])
+            complete(urlpath + "/" + volume["_id"] + "/" + book["_id"] + "/" + chapter["_id"].removeprefix(book["_id"]), volume["_id"] + "/" + book["_id"] + "/" + chapter["_id"])
